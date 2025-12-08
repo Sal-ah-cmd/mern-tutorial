3f327281466d4cff0fcc12db334f00a7f2d68f91
Salah <187458641+Sal-ah-cmd@users.noreply.github.com>
2025-12-08 02:40:45 -0500

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+// frontend/src/App.jsx (CORRECT STRUCTURE)
+
+import { Route, Routes } from "react-router-dom"; 
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx"; 
+import { AuthProvider } from "./context/AuthContext.jsx"; 
+import Navbar from "./components/Navbar.jsx"; 
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Navbar />
+      <div className="relative h-full w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+          <Route path="/login" element={<LoginPage />} /> 
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
+};
+
+export default App;
