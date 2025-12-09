98e0226790804948b1e7b1263183f79e96c5f20f
Salah <187458641+Sal-ah-cmd@users.noreply.github.com>
2025-12-08 23:27:58 -0500

    Add files via upload

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+// backend/src/routes/userRoutes.js
+
+import express from 'express';
+import { authUser, registerUser } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/login', authUser);
+router.post('/register', registerUser);
+
+export default router;
