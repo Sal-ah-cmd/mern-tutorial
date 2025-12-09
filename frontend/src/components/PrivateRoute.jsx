7d464bd97e065c6198e96c3e773ff45c954b41db
Salah <187458641+Sal-ah-cmd@users.noreply.github.com>
2025-12-09 16:35:26 -0500

    Delete frontend/src directory

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext.jsx";
-
-const PrivateRoute = () => {
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
